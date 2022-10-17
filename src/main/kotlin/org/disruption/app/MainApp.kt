427538c061a3658d836b1e6b7660b086cfc1cea6
Dennis Yilmaz <dy.0x8@outlook.com>
2022-10-17 02:05:59 -0400

    Compatibility Work

--- src/main/kotlin/org/disruption/app/MainApp.kt
+++ src/main/kotlin/org/disruption/app/MainApp.kt
@@ -1,11 +1,10 @@
 package org.disruption.app
 
-import
 import org.disruption.app.view.MainView
 import tornadofx.App;
 
 class MainApp: App(MainView::class, Styles::class) {
     companion object {
-        val runtime = ElapsedTime()
+
     }
 }
--- src/main/kotlin/org/disruption/app/view/MainViewController.kt
+++ src/main/kotlin/org/disruption/app/view/MainViewController.kt
@@ -1,6 +1,6 @@
 package org.disruption.app.view
 
-import org.firstinspires.ftc.robotcore.network.NetworkConnectionHandler
+// import org.firstinspires.ftc.robotcore.network.NetworkConnectionHandler
 import tornadofx.*
 import java.net.InetAddress
 import java.net.UnknownHostException
@@ -9,7 +9,7 @@ class MainViewController : Controller() {
     fun connect(addressString: String) {
         try {
             val address = InetAddress.getByName(addressString)
-            NetworkConnectionHandler.getInstance().init(address)
+            // NetworkConnectionHandler.getInstance().init(address)
         } catch (e: UnknownHostException) {
             // TODO: display an error
         }
